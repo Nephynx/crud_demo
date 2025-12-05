8e7cc479f28fe29dd23968fdb897b858084a1916
Nephynx <Saxtonjl58@gmail.com>
2025-12-05 09:28:23 +0800

    COMMIT

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,108 +22,210 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btnConnect = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ButtonConnect = New System.Windows.Forms.Button()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.TextBoxAge = New System.Windows.Forms.TextBox()
+        Me.TextBoxEmail = New System.Windows.Forms.TextBox()
+        Me.lblName = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.btnInsert = New System.Windows.Forms.Button()
+        Me.ButtonInsert = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ButtonRead = New System.Windows.Forms.Button()
+        Me.ButtonUpdate = New System.Windows.Forms.Button()
+        Me.ButtonDelete = New System.Windows.Forms.Button()
+        Me.TextBoxHiddenID = New System.Windows.Forms.TextBox()
+        Me.ButtonViewDeleted = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'btnConnect
+        'ButtonConnect
         '
-        Me.btnConnect.Location = New System.Drawing.Point(12, 12)
-        Me.btnConnect.Name = "btnConnect"
-        Me.btnConnect.Size = New System.Drawing.Size(187, 104)
-        Me.btnConnect.TabIndex = 0
-        Me.btnConnect.Text = "Connect MySQL"
-        Me.btnConnect.UseVisualStyleBackColor = True
+        Me.ButtonConnect.Font = New System.Drawing.Font("Bahnschrift Condensed", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonConnect.Location = New System.Drawing.Point(12, 12)
+        Me.ButtonConnect.Name = "ButtonConnect"
+        Me.ButtonConnect.Size = New System.Drawing.Size(535, 104)
+        Me.ButtonConnect.TabIndex = 0
+        Me.ButtonConnect.Text = "Connect MySQL"
+        Me.ButtonConnect.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'TextBoxName
         '
-        Me.TextBox1.Location = New System.Drawing.Point(61, 122)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.TextBoxName.Location = New System.Drawing.Point(45, 122)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(502, 20)
+        Me.TextBoxName.TabIndex = 1
         '
-        'TextBox2
+        'TextBoxAge
         '
-        Me.TextBox2.Location = New System.Drawing.Point(61, 148)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 1
+        Me.TextBoxAge.Location = New System.Drawing.Point(45, 148)
+        Me.TextBoxAge.Name = "TextBoxAge"
+        Me.TextBoxAge.Size = New System.Drawing.Size(502, 20)
+        Me.TextBoxAge.TabIndex = 1
         '
-        'TextBox3
+        'TextBoxEmail
         '
-        Me.TextBox3.Location = New System.Drawing.Point(61, 174)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox3.TabIndex = 1
+        Me.TextBoxEmail.Location = New System.Drawing.Point(45, 174)
+        Me.TextBoxEmail.Name = "TextBoxEmail"
+        Me.TextBoxEmail.Size = New System.Drawing.Size(502, 20)
+        Me.TextBoxEmail.TabIndex = 1
         '
-        'Label1
+        'lblName
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 128)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Label1"
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.Transparent
+        Me.lblName.Font = New System.Drawing.Font("High Tower Text", 9.0!)
+        Me.lblName.ForeColor = System.Drawing.Color.Black
+        Me.lblName.Location = New System.Drawing.Point(3, 124)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(36, 15)
+        Me.lblName.TabIndex = 2
+        Me.lblName.Text = "Name"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(13, 151)
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("High Tower Text", 9.0!)
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(5, 152)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.Size = New System.Drawing.Size(27, 15)
         Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Label1"
+        Me.Label2.Text = "Age"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 177)
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("High Tower Text", 9.0!)
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(5, 178)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.Size = New System.Drawing.Size(34, 15)
         Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Label1"
+        Me.Label3.Text = "Email"
         '
-        'btnInsert
+        'ButtonInsert
         '
-        Me.btnInsert.Location = New System.Drawing.Point(12, 205)
-        Me.btnInsert.Name = "btnInsert"
-        Me.btnInsert.Size = New System.Drawing.Size(187, 23)
-        Me.btnInsert.TabIndex = 3
-        Me.btnInsert.Text = "Insert Data"
-        Me.btnInsert.UseVisualStyleBackColor = True
+        Me.ButtonInsert.Font = New System.Drawing.Font("Cambria", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonInsert.Location = New System.Drawing.Point(12, 225)
+        Me.ButtonInsert.Name = "ButtonInsert"
+        Me.ButtonInsert.Size = New System.Drawing.Size(535, 23)
+        Me.ButtonInsert.TabIndex = 3
+        Me.ButtonInsert.Text = "Insert Data"
+        Me.ButtonInsert.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Cambria", 8.25!)
+        Me.btnExit.Location = New System.Drawing.Point(12, 497)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(535, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.DimGray
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.GridColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 341)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(535, 150)
+        Me.DataGridView1.TabIndex = 5
+        '
+        'ButtonRead
+        '
+        Me.ButtonRead.Font = New System.Drawing.Font("Cambria", 8.25!)
+        Me.ButtonRead.Location = New System.Drawing.Point(12, 254)
+        Me.ButtonRead.Name = "ButtonRead"
+        Me.ButtonRead.Size = New System.Drawing.Size(535, 23)
+        Me.ButtonRead.TabIndex = 6
+        Me.ButtonRead.Text = "Read Data"
+        Me.ButtonRead.UseVisualStyleBackColor = True
+        '
+        'ButtonUpdate
+        '
+        Me.ButtonUpdate.Font = New System.Drawing.Font("Cambria", 8.25!)
+        Me.ButtonUpdate.Location = New System.Drawing.Point(12, 283)
+        Me.ButtonUpdate.Name = "ButtonUpdate"
+        Me.ButtonUpdate.Size = New System.Drawing.Size(535, 23)
+        Me.ButtonUpdate.TabIndex = 7
+        Me.ButtonUpdate.Text = "Update Data"
+        Me.ButtonUpdate.UseVisualStyleBackColor = True
+        '
+        'ButtonDelete
+        '
+        Me.ButtonDelete.Font = New System.Drawing.Font("Cambria", 8.25!)
+        Me.ButtonDelete.Location = New System.Drawing.Point(12, 312)
+        Me.ButtonDelete.Name = "ButtonDelete"
+        Me.ButtonDelete.Size = New System.Drawing.Size(535, 23)
+        Me.ButtonDelete.TabIndex = 7
+        Me.ButtonDelete.Text = "Delete Data"
+        Me.ButtonDelete.UseVisualStyleBackColor = True
+        '
+        'TextBoxHiddenID
+        '
+        Me.TextBoxHiddenID.Location = New System.Drawing.Point(45, 200)
+        Me.TextBoxHiddenID.Name = "TextBoxHiddenID"
+        Me.TextBoxHiddenID.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxHiddenID.TabIndex = 8
+        '
+        'ButtonViewDeleted
+        '
+        Me.ButtonViewDeleted.Font = New System.Drawing.Font("Cambria", 8.25!)
+        Me.ButtonViewDeleted.Location = New System.Drawing.Point(12, 526)
+        Me.ButtonViewDeleted.Name = "ButtonViewDeleted"
+        Me.ButtonViewDeleted.Size = New System.Drawing.Size(535, 23)
+        Me.ButtonViewDeleted.TabIndex = 9
+        Me.ButtonViewDeleted.Text = "View Deleted"
+        Me.ButtonViewDeleted.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.btnInsert)
+        Me.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.ClientSize = New System.Drawing.Size(559, 572)
+        Me.Controls.Add(Me.ButtonViewDeleted)
+        Me.Controls.Add(Me.TextBoxHiddenID)
+        Me.Controls.Add(Me.ButtonDelete)
+        Me.Controls.Add(Me.ButtonUpdate)
+        Me.Controls.Add(Me.ButtonRead)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.ButtonInsert)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.TextBoxEmail)
+        Me.Controls.Add(Me.TextBoxAge)
+        Me.Controls.Add(Me.TextBoxName)
+        Me.Controls.Add(Me.ButtonConnect)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = " "
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents btnConnect As Button
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents Label1 As Label
+    Friend WithEvents ButtonConnect As Button
+    Friend WithEvents TextBoxName As TextBox
+    Friend WithEvents TextBoxAge As TextBox
+    Friend WithEvents TextBoxEmail As TextBox
+    Friend WithEvents lblName As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents btnInsert As Button
+    Friend WithEvents ButtonInsert As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ButtonRead As Button
+    Friend WithEvents ButtonUpdate As Button
+    Friend WithEvents ButtonDelete As Button
+    Friend WithEvents TextBoxHiddenID As TextBox
+    Friend WithEvents ButtonViewDeleted As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,21 +1,130 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class Form1
     Dim conn As MySqlConnection
-    Dim Commands As MySqlCommand
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Dim COMMAND As MySqlCommand
 
-    End Sub
-
-    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+    Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
         conn = New MySqlConnection
-        conn.ConnectionString = "server=localhost; userid=root;password=root;"
+        conn.ConnectionString = "server=localhost; userid=root; password=root; database=crud_demo_db;"
 
         Try
             conn.Open()
             MessageBox.Show("Connected")
         Catch ex As Exception
+            MessageBox.Show(ex.Message)
             conn.Close()
+        End Try
+    End Sub
 
+    Private Sub ButtonCreate_Click(sender As Object, e As EventArgs) Handles ButtonInsert.Click
+        Dim query As String = "INSERT INTO `crud_demo_db`.`students_tbl` (`name`, `age`, `email`) VALUES (@name, @age, @email);"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@name", TextBoxName.Text)
+                    cmd.Parameters.AddWithValue("@age", CInt(TextBoxAge.Text))
+                    cmd.Parameters.AddWithValue("@email", TextBoxEmail.Text)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record insert successful!")
+                    TextBoxName.Clear()
+                    TextBoxAge.Clear()
+                    TextBoxEmail.Clear()
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonRead_Click(sender As Object, e As EventArgs) Handles ButtonRead.Click
+        'Dim query As String = "SELECT * FROM crud_demo_db.students_tbl;"
+        Dim query As String = "SELECT * FROM crud_demo_db.students_tbl WHERE is_deleted = 0;"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
+                Dim adapter As New MySqlDataAdapter(query, conn)
+                Dim table As New DataTable()
+                adapter.Fill(table)
+                DataGridView1.DataSource = table
+                DataGridView1.Columns("id").Visible = False
+                DataGridView1.Columns("is_deleted").Visible = False
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonUpdate_Click(sender As Object, e As EventArgs) Handles ButtonUpdate.Click
+        If DataGridView1.SelectedRows.Count = 0 Then
+            MessageBox.Show("Select a row to update.")
+        End If
+        Dim row As DataGridViewRow = DataGridView1.SelectedRows(0)
+        Dim Id As Integer = row.Cells("id").Value
+
+        Dim query As String = "UPDATE student_tbl SET name = @name, age = @age, email = @email WHERE id = @id"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@name", TextBoxName.Text)
+                    cmd.Parameters.AddWithValue("@age", CInt(TextBoxAge.Text))
+                    cmd.Parameters.AddWithValue("@email", TextBoxEmail.Text)
+                    cmd.Parameters.AddWithValue("@id", Id)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record updated successfully!")
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonDelete_Click(sender As Object, e As EventArgs) Handles ButtonDelete.Click
+        Dim query As String = "DELETE FROM `crud_demo_db`.`students_tbl` WHERE (`id` = @id) SET 'is_deleted' = 1;"
+        'Dim query As String = "UPDATE `crud_demo_db`.`students_tbl` SET `is_deleted` = 1 WHERE (`id` = @id);"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@id", CInt(TextBoxHiddenID.Text))
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record deleted successfully!")
+                    TextBoxName.Clear()
+                    TextBoxAge.Clear()
+                    TextBoxEmail.Clear()
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Environment.Exit(0)
+    End Sub
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        If e.RowIndex >= 0 Then
+            Dim selectedRow As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+            TextBoxName.Text = selectedRow.Cells("name").Value.ToString()
+            TextBoxAge.Text = selectedRow.Cells("age").Value.ToString()
+            TextBoxEmail.Text = selectedRow.Cells("email").Value.ToString()
+            TextBoxHiddenID.Text = selectedRow.Cells("id").Value.ToString()
+        End If
+    End Sub
+
+    Private Sub ButtonViewDeleted_Click(sender As Object, e As EventArgs) Handles ButtonViewDeleted.Click
+        Dim query As String = "SELECT * FROM crud_demo_db.students_tbl WHERE is_deleted = 1;"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
+                Dim adapter As New MySqlDataAdapter(query, conn)
+                Dim table As New DataTable()
+                adapter.Fill(table)
+                DataGridView1.DataSource = table
+                DataGridView1.Columns("id").Visible = False
+                DataGridView1.Columns("is_deleted").Visible = False
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
         End Try
     End Sub
 End Class
